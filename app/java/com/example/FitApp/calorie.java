8c1fb2d3aaa9ebd8b5e9a4a3c0e62ee8bff01030
tderemer <60191559+tderemer@users.noreply.github.com>
2020-04-26 01:04:20 -0400

    Add files via upload
    Finalized??

---
+++ app/java/com/example/FitApp/calorie.java
@@ -0,0 +1,68 @@
+package com.example.FitApp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+
+public class calorie extends Activity {
+
+    //PRIVATIZE INFORMATION
+    private TextView totalcals;
+    private float num1,num2,num3,num4,num5;
+    private Button calorietotal, reset;
+    private float runningtotal = 0;
+    private EditText breakfastcals, lunchcals, dinnercals, snackcals,drinkcals;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.calories_page);
+
+
+        //INITIALIZE UI CONNECTION
+        calorietotal = (Button) findViewById(R.id.calorietotal);
+        reset = (Button) findViewById(R.id.reset);
+        breakfastcals = (EditText) findViewById(R.id.breakfastcals);
+        lunchcals = (EditText) findViewById(R.id.lunchcals);
+        dinnercals = (EditText) findViewById(R.id.dinnercals);
+        snackcals = (EditText) findViewById(R.id.snackcals);
+        drinkcals = (EditText) findViewById(R.id.drinkcals);
+        totalcals = (TextView) findViewById(R.id.totalcals);
+
+        //PROCESS CALCULATION
+        calorietotal.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                num1 = Integer.parseInt(breakfastcals.getText().toString());
+                num2 = Integer.parseInt(lunchcals.getText().toString());
+                num3 = Integer.parseInt(dinnercals.getText().toString());
+                num4 = Integer.parseInt(snackcals.getText().toString());
+                num5 = Integer.parseInt(drinkcals.getText().toString());
+                runningtotal = num1 + num2 + num3 + num4 + num5;
+                totalcals.setText(String.valueOf(runningtotal));
+            }
+        });
+
+        //RESET BUTTON
+        reset.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                runningtotal = 0;
+                totalcals.setText(String.valueOf(runningtotal));
+            }
+        });
+
+
+        Intent activityThatCalled = getIntent();
+        String previousActivity = activityThatCalled.getExtras().getString("callingActivity");
+
+
+
+    }
+}
