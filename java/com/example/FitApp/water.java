26528d7e368d3b5423ab85c8c760a069c2a765b6
tderemer <60191559+tderemer@users.noreply.github.com>
2020-04-26 10:46:53 -0400

    Add files via upload
    @Zen, changes you requested.

---
+++ java/com/example/FitApp/water.java
@@ -0,0 +1,84 @@
+package com.example.FitApp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class water extends Activity {
+
+    //PRIVATIZE VARIABLES
+    private TextView water_total;
+    private float num1;
+    private float runningtotal = 0;
+    private Button oz8, oz16, oz20, oz32, reset;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.water);
+
+        //INITIATE THE UI CONNECTION
+        oz8 = (Button) findViewById(R.id.oz8);
+        oz16 = (Button) findViewById(R.id.oz16);
+        oz20 = (Button) findViewById(R.id.oz20);
+        oz32 = (Button) findViewById(R.id.oz32);
+        reset = (Button) findViewById(R.id.reset);
+        water_total = (TextView) findViewById(R.id.water_total) ;
+
+
+        //SETUP ALL BUTTONS
+        oz8.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+
+                num1 = 8;
+                runningtotal = runningtotal + num1;
+                String temp_string = String.valueOf(runningtotal*29.57) + "ml";
+                water_total.setText(temp_string);
+            }
+        });
+        oz16.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+
+                num1 = 17;
+                runningtotal = runningtotal + num1;
+                String temp_string = String.valueOf(runningtotal*29.57) + "ml";
+                water_total.setText(temp_string);
+            }
+        });
+        oz20.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+
+                num1 = 20;
+                runningtotal = runningtotal + num1;
+                String temp_string = String.valueOf(runningtotal*29.57) + "ml";
+                water_total.setText(temp_string);
+            }
+        });
+        oz32.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+
+                num1 = 32;
+                runningtotal = runningtotal + num1;
+                String temp_string = String.valueOf(runningtotal*29.57) + "ml";
+                water_total.setText(temp_string);
+            }
+        });
+
+        //RESET BUTTON
+        reset.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                runningtotal = 0;
+                water_total.setText(String.valueOf(runningtotal) + "ml");
+            }
+        });
+
+
+        Intent activityThatCalled = getIntent();
+        String previousActivity = activityThatCalled.getExtras().getString("callingActivity");
+    }
+}
